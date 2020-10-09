b62cc33847dce403117c4c16155756736dcd44fc
Axteli <contact.axteli@gmail.com>
2020-10-09 19:07:19 +0200

    Command  added

--- index.js
+++ index.js
@@ -35,4 +35,18 @@ client.on('message', (message) => {
     }       
 })
 
+client.on('message', (message) => {
+    if (message.content === `${prefix}poubelle`){
+        const poubelle = new Discord.MessageEmbed()
+         .setTitle("La poubelle")
+         .setColor("#527a9e")
+         .setDescription("La <#756273846092300370> permet de détruire des formes inutiles. Elle est souvent utililisée après avoir mis des formes dans un <#756273811598344344>, puis détruire la partie dont vous n’avez pas besoin.")
+         .addField('Détails', "Item/s : infini\nTaille : 1x1\nRaccourci : 0\n Entrée : 4\nSortie : 0", false)
+         .setThumbnail('https://cdn.discordapp.com/attachments/758707064251482162/764153209202671616/trash.png')
+         .setFooter('Infos tirées du fandom', 'https://cdn.discordapp.com/attachments/758707064251482162/759150748276621338/1_TtT_gbfvQ5LitUE9wfpkog.png')
+    message.channel.send(poubelle);
+    console.log('commande poubelle executé');
+    }       
+})
+
 client.login(token);
