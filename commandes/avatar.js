c8a0b808f7505cf670e839618b44d62c31d5f3fa
Axteli <contact.axteli@gmail.com>
2020-10-11 18:06:48 +0200

    Avatar functional with gif + unban functional

--- commandes/avatar.js
+++ commandes/avatar.js
@@ -4,8 +4,8 @@ module.exports.run = async(bot, message, args) => {
     if(!message.mentions.users.first()){
         const avatar = new Discord.MessageEmbed()
          .setTitle("Ton avatar")
-         .setURL(message.author.displayAvatarURL())
-         .setImage(message.author.displayAvatarURL())
+         .setURL(message.author.displayAvatarURL({ format: 'png', dynamic: true, size: 1024 }))
+         .setImage(message.author.displayAvatarURL({ format: 'png', dynamic: true, size: 1024 }))
          .setDescription("Voici ton avatar")
          .setColor("#527a9e")
         return message.channel.send(avatar)
@@ -13,13 +13,14 @@ module.exports.run = async(bot, message, args) => {
         const user = message.mentions.users.first()
         const avatar1 = new Discord.MessageEmbed()
          .setTitle(`Avatar de ${user.tag}`)
-         .setURL(user.displayAvatarURL())
-         .setImage(user.displayAvatarURL())
+         .setURL(user.displayAvatarURL({ format: 'png', dynamic: true, size: 1024 }))
+         .setImage(user.displayAvatarURL({ format: 'png', dynamic: true, size: 1024 }))
          .setDescription(`Voici l'avatar de ${user.tag}`)
          .setColor("#527a9e")
+        console.log(`commande : patron | par : ${message.author} | dans : ${message.channel} (${message.channel.id})| serveur : ${message.guild} (${message.guild.id})`)
         return message.channel.send(avatar1)
+        
     }
-
 }
 
 module.exports.help = {
