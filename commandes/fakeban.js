0cc9a0437ec240704d9864acbad654b2c0c4dd6e
Axteli <contact.axteli@gmail.com>
2020-10-11 16:33:27 +0200

    Command Fakeban functional

---
+++ commandes/fakeban.js
@@ -0,0 +1,17 @@
+const Discord = require('discord.js')
+module.exports.run = async(bot, message, args) => {
+    
+    let user = message.mentions.members.first()
+    message.delete();
+    if (!args[0]) {
+        return message.channel.send(`Tu ne m'as pas dit qui je dois bannir !`)
+    }
+    message.channel.send(
+      `${user} a bien été banni !` 
+    );
+    console.log(`commande : fakeban | par : ${message.author} | dans : ${message.channel} (${message.channel.id})| serveur : ${message.guild} (${message.guild.id})`)
+}
+
+module.exports.help = {
+name: "fakeban"
+}
