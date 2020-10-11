968e27af65af630a579a77b1d231d8610abee70f
Axteli <contact.axteli@gmail.com>
2020-10-11 18:21:27 +0200

    Kick functional

---
+++ commandes/kick.js
@@ -0,0 +1,24 @@
+const Discord = require('discord.js')
+module.exports.run = async(bot, message, args) => {
+
+
+ if (!message.member.hasPermission('KICK_MEMBERS')) {
+    return message.channel.send(`Tu n'as pas la permission de kick!`)
+}
+if (!args[0]) {
+    return message.channel.send(`Mentionne quelqu'un !`)
+}
+const member = message.mentions.members.first() || message.guild.members.cache.get(args[0]);
+
+try {
+    await member.kick();
+    await message.channel.send(`${member} a été kické!`)
+} catch (e) {
+    return message.channel.send(`Cet utilisateur n'est pas sur le serveur!`)
+}
+
+}
+
+module.exports.help = {
+name: "kick"
+}
