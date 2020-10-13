b67eca9217df17aa03be4bff09a2677ec2c73fd8
Axteli <contact.axteli@gmail.com>
2020-10-13 18:31:06 +0200

    status update

--- index.js
+++ index.js
@@ -39,12 +39,12 @@ bot.on('ready', () => {
         console.log(" - " + guild.name + " - " + guild.id)})})
 
 bot.on('ready', function(){
-                    bot.user.setActivity("racketter Kaito", {type: "PLAYING"})
+                    bot.user.setActivity("Shapez.io | $help", {type: "PLAYING"})         
                     log(chalk.bgRed('Chargement du bot en cours... '));})
                     bot.on('guildCreate' , async guild => {
-                    bot.user.setActivity("racketter Kaito", {type: "PLAYING"})})
+                    bot.user.setActivity("Shapez.io | $help", {type: "PLAYING"})})
                     bot.on('guildDelete', async guild => {
-                    bot.user.setActivity("racketter Kaito", {type: "PLAYING"})})
+                    bot.user.setActivity("Shapez.io | $help", {type: "PLAYING"})})
 
 bot.login(config.token)
   bot.on('message', async message => {
