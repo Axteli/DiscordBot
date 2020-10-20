28f6abf4bde0080912250ae102db34b9365989f4
Axteli <contact.axteli@gmail.com>
2020-10-20 13:08:44 +0200

    logs on start added

--- index.js
+++ index.js
@@ -37,6 +37,14 @@ bot.on('ready', () => {
     console.log("Servers:")
     bot.guilds.cache.forEach((guild) => {
         console.log(" - " + guild.name + " - " + guild.id)})})
+        const embed = new Discord.MessageEmbed()
+        .setColor('GREEN')
+        .setTitle('<:online:764845529380290581>')
+        .setDescription('Le bot a bien été allumé !')
+        const webhookClient = new Discord.WebhookClient(config.webhookID, config.webhookToken);
+          webhookClient.send('', {
+            embeds: [embed],
+        });
 
 bot.on('ready', function(){
                     bot.user.setActivity("Shapez.io | $help", {type: "PLAYING"})         
