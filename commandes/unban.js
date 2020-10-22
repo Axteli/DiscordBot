a4515f03c526a8eea98fae17d64b6ece3c7cf640
Axteli <contact.axteli@gmail.com>
2020-10-22 12:20:29 +0200

    Unban api error fix

--- commandes/unban.js
+++ commandes/unban.js
@@ -9,12 +9,11 @@ module.exports.run = async(bot, message, args) => {
     if (!member) {
          return message.channel.send(`merci de donner un id!`)
     }
-
+    if(!member.unbannable) return message.channel.send('Une erreur c\'est produite. Soit car l\'id est invalide, soit car cette personne n\'est pas banni.'),console.log(`commande : unban | par : ${message.author.tag} (${message.author.id}) | dans : ${message.channel.name} (${message.channel.id})| serveur : ${message.guild} (${message.guild.id})| détails : la personne ne peut pas etre unbanni`)
     try {
         message.guild.fetchBans().then(bans => {
             message.guild.members.unban(member)
         })
-        if(!member.unbannable) return message.channel.send('Une erreur c\'est produite. Soit car l\'id est invalide, soit car cette personne n\'est pas banni.'),console.log(`commande : unban | par : ${message.author.tag} (${message.author.id}) | dans : ${message.channel.name} (${message.channel.id})| serveur : ${message.guild} (${message.guild.id})| détails : la personne ne peut pas etre unbanni`)
         await message.channel.send(`${member} a été unban !`)
         await console.log(`commande : unban | par : ${message.author.tag} (${message.author.id}) | dans : ${message.channel.name} (${message.channel.id})| serveur : ${message.guild} (${message.guild.id})| membre visé : ${member}`)
     } catch (e) {
