bb1fa853e84f0b5aaaa3769001a01d1a3f369ad9
Axteli <contact.axteli@gmail.com>
2020-10-23 23:02:40 +0200

    Sat update

--- commandes/say.js
+++ commandes/say.js
@@ -1,16 +1,20 @@
 const Discord = require('discord.js')
 module.exports.run = async(bot, message, args) => {
 
+    message.delete();
+     
+    //définir text qui est le texte que l'on vas envoyer
+        var text = message.content.split(' ').slice(1).join(' ');
 
-     {
-      var text = message.content.split(' ').slice(1).join(' ');
-      if(!text) return message.channel.send('Please use it like this example:\n**!say Bottom Text**');
-       message.channel.send(text);
-      message.delete();
-      message.channel.stopTyping()
-      console.log(`commande : say | par : ${message.author.tag} (${message.author.id}) | dans : ${message.channel.name} (${message.channel.id})| serveur : ${message.guild} (${message.guild.id})| texte : ${text}`)
-     }
-    };
+    //si aucun texte défini return
+        if(!text) return message.channel.send(`<a:tickred:764793956813766687> Erreur | ${message.author.tag}, tu ne m\'as pas dit ce que je devais écrire!`);
+    
+    //envoyer le text défini plus haut
+        message.channel.send(text);
+
+    console.log(`commande : say | par : ${message.author.tag} (${message.author.id}) | dans : ${message.channel.name} (${message.channel.id})| serveur : ${message.guild} (${message.guild.id})| texte : ${text}`)
+
+}
     
 
 module.exports.help = {
