6473bf38ab1249fed111bc0eeeb83fb0c624b6c4
Axteli <contact.axteli@gmail.com>
2020-10-23 19:38:32 +0200

    Clear update

--- commandes/clear.js
+++ commandes/clear.js
@@ -1,15 +1,27 @@
 const Discord = require('discord.js')
 module.exports.run = async(bot, message, args) => {
 
-await message.delete();
-if (!message.member.permissions.has("MANAGE_MESSAGES")) // sets the permission
-            return message.channel.send(
-                `${message.author.username}, tu n'as pas la permission de faire ça` // returns this message to user with no perms
-            );
-        if (!args[0]) {
-            return message.channel.send(`Merci d'entrer un chiffre entre 1 et 100`)
-        }
+    message.delete();
 
+//vérifie les permissions
+    if (!message.member.hasPermission("MANAGE_MESSAGES")) 
+        return message.channel.send(`<a:tickred:764793956813766687> Erreur | ${message.author.username}, tu n'as pas la permission "gérer les messages"!`,
+         console.log(`commande : clear | par : ${message.author.tag} (${message.author.id}) | dans : ${message.channel.name} (${message.channel.id})| serveur : ${message.guild} (${message.guild.id})| détails : ${message.author.username} n'as pas la permission gérer les messages.`)
+        );
+
+    if (!message.guild.bot.hasPermission("MANAGE_MESSAGES"))
+        return message.channel.send(`<a:tickred:764793956813766687> Erreur | ${message.author.username}, je n'est pas la permission "gérer les message"!`,
+         console.log(`commande : clear | par : ${message.author.tag} (${message.author.id}) | dans : ${message.channel.name} (${message.channel.id})| serveur : ${message.guild} (${message.guild.id})| détails : le bot n'as pas la permission gérer les messages.`)
+        );
+
+
+//vérifier qu'un nombre est entré
+        if (!args[0]) {
+            return message.channel.send(`<a:tickred:764793956813766687> Erreur | ${message.author.username}, merci d'entrer un nombre entre 1 et 100`),
+             console.log(`commande : clear | par : ${message.author.tag} (${message.author.id}) | dans : ${message.channel.name} (${message.channel.id})| serveur : ${message.guild} (${message.guild.id})| détails : aucun nombre rentré.`)
+        };
+
+//faire des calculs relou (⊙_⊙;)
         let deleteAmount;
 
         if (parseInt(args[0]) > 100 ) {
@@ -20,16 +32,21 @@ if (!message.member.permissions.has("MANAGE_MESSAGES")) // sets the permission
 
         message.channel.bulkDelete(deleteAmount, true);
 
+//envoyé l'embed de finalisation
         const clear = new Discord.MessageEmbed()
-            .setTitle(`${message.author.username}`)
-            .setDescription(`<a:tickgreen:764793938317803551> ${deleteAmount} messages ont été supprimés !`)
-            .setFooter(message.author.username, message.author.displayAvatarURL())
-            .setColor("#527a9e")
-            const msg = await message.channel.send(clear)
-            await setTimeout(() => { msg.delete();}, 3000)
-        console.log(`commande : clear | par : ${message.author.tag} (${message.author.id}) | dans : ${message.channel.name} (${message.channel.id})| serveur : ${message.guild} (${message.guild.id})| nb de msg supprimés : ${deleteAmount}`)
-    }
 
+         .setTitle(`${message.author.username}`)
+         .setDescription(`<a:tickgreen:764793938317803551> ${deleteAmount} messages ont été supprimés !`)
+         .setFooter(message.author.username, message.author.displayAvatarURL())
+         .setColor("#527a9e")
+
+        const msg = await message.channel.send(clear)
+
+//supprime l'embed au bout de 3sec
+        await setTimeout(() => { msg.delete();}, 3000)
+        console.log(`commande : clear | par : ${message.author.tag} (${message.author.id}) | dans : ${message.channel.name} (${message.channel.id})| serveur : ${message.guild} (${message.guild.id})| nb de msg supprimés : ${deleteAmount}`)
+    
+}
 
 module.exports.help = {
 name: "clear"
