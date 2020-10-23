d409b83ec4eb08abadf62c57f49e558cf367b57a
Axteli <contact.axteli@gmail.com>
2020-10-23 22:57:38 +0200

    Error message for ban fix

--- commandes/ban.js
+++ commandes/ban.js
@@ -32,22 +32,22 @@ module.exports.run = async(bot, message, args) => {
 
     //vérifie qui est la personne a ban
         if(member.id === '761914312422981632') 
-            return message.channel.send(`${message.author}, pourquoi veut tu me ban? tu ne m\'aime pas? :cry:`),
+            return message.channel.send(`${message.author.username}, pourquoi veut tu me ban? tu ne m\'aime pas? :cry:`),
              console.log(`commande : ban | par : ${message.author.tag} (${message.author.id}) | dans : ${message.channel.name} (${message.channel.id})| serveur : ${message.guild} (${message.guild.id})| détails : a essayé de ban le bot`)
 
         if(member.id === message.author) 
-            return message.channel.send(`${message.author}, tu ne peux pas te ban toi-meme!`),
+            return message.channel.send(`<a:tickred:764793956813766687> Erreur | ${message.author.username}, tu ne peux pas te ban toi-meme!`),
              console.log(`commande : ban | par : ${message.author.tag} (${message.author.id}) | dans : ${message.channel.name} (${message.channel.id})| serveur : ${message.guild} (${message.guild.id})| détails : ${message.author.username} a essayé de se ban sois-meme`)
 
 
     //verifie si la personne est banable
         if(!member.banable) 
-            return message.channel.send(`<a:tickred:764793956813766687> Erreur | ${message.author}, je ne peux pas bannir cette personne probablement car elle a un rôle au dessus du miens`),
+            return message.channel.send(`<a:tickred:764793956813766687> Erreur | ${message.author.username}, je ne peux pas bannir cette personne probablement car elle a un rôle au dessus du miens`),
              console.log(`commande : ban | par : ${message.author.tag} (${message.author.id}) | dans : ${message.channel.name} (${message.channel.id})| serveur : ${message.guild} (${message.guild.id})| détails : la personne ne peut pas etre kick par le bot`)
 
     //vérifie si la personne à ban n'est pas plus haut gradé
         if (message.member.roles.highest.comparePositionTo(member.roles.highest) < 1 && message.author.id !== message.guild.ownerID) 
-            return message.channel.send(`${message.author.username}, tu ne peux pas ban quelqu'un plus haut gradé que toi!`)
+            return message.channel.send(`<a:tickred:764793956813766687> Erreur | ${message.author.username}, tu ne peux pas ban quelqu'un plus haut gradé que toi!`)
              console.log(`commande : ban | par : ${message.author.tag} (${message.author.id}) | dans : ${message.channel.name} (${message.channel.id})| serveur : ${message.guild} (${message.guild.id})| détails : a essayé de ban un membre plus haut gradé`)
 
 
@@ -63,7 +63,7 @@ module.exports.run = async(bot, message, args) => {
         
          .setColor("#527a9e")
          .setTitle(`Tu a été ban du serveur : ${message.guild}`)
-         .setDescription(`Par : ${message.author}\nRaison : ${reason}`)
+         .setDescription(`Par : ${message.author.tag}\nRaison : ${reason}`)
          .setThumbnail(message.guild.iconURL())
          .setTimestamp()
 
@@ -79,7 +79,7 @@ module.exports.run = async(bot, message, args) => {
          .setColor("#527a9e")
          .setTitle('Membre banni <a:bancat:764793999130886194>')
          .setThumbnail(member.user.displayAvatarURL())
-         .setDescription(`Membre banni : ${member}\nPar : ${message.author}\nRaison : ${reason}`)
+         .setDescription(`Membre banni : ${member}\nPar : ${message.author.tag}\nRaison : ${reason}`)
      
         message.channel.send(banembed);
 
