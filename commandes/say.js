e0691e22c21264ddb8850952be109e1afae11748
Axteli <contact.axteli@gmail.com>
2020-10-26 08:08:36 +0100

    Permission require for Say added

--- commandes/say.js
+++ commandes/say.js
@@ -3,6 +3,12 @@ module.exports.run = async(bot, message, args) => {
 
     message.delete();
      
+    //vérifie la permission du membre
+        if (!message.member.hasPermission('ADMINISTRATOR')) {
+            return message.channel.send(`<a:tickred:764793956813766687> Erreur | ${message.author.username}, il te manque la permission "administrateur" pour effecctuer cette commande!`),
+             console.log(`commande : say | par : ${message.author.tag} (${message.author.id}) | dans : ${message.channel.name} (${message.channel.id})| serveur : ${message.guild} (${message.guild.id})| détails : ${message.author.name} n'as pas la permission admin`)
+        }
+
     //définir text qui est le texte que l'on vas envoyer
         var text = message.content.split(' ').slice(1).join(' ');
 
