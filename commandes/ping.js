19a86c675eb16c279ffd30121beb645b6a8913c5
Axteli <contact.axteli@gmail.com>
2020-10-26 08:06:24 +0100

    Ping update

--- commandes/ping.js
+++ commandes/ping.js
@@ -1,14 +1,16 @@
 const Discord = require('discord.js')
 module.exports.run = async(bot, message, args) => {
+    
+    message.delete();
 
     const embedPing = new Discord.MessageEmbed()
-      .setColor("RED")
-      .setDescription(`:ping_pong: Latence du bot : ${Date.now() - message.createdTimestamp}ms \n :heart: Latence de l'API : ${Math.round(bot.ws.ping)}ms`)
+     .setColor("#527a9e")
+     .setDescription(`:ping_pong: Latence du bot : ${Date.now() - message.createdTimestamp}ms \n :heart: Latence de l'API : ${Math.round(bot.ws.ping)}ms`)
     message.channel.send(embedPing)
-    message.delete();
+    
     console.log(`commande : ping | par : ${message.author.tag} (${message.author.id}) | dans : ${message.channel.name} (${message.channel.id})| serveur : ${message.guild} (${message.guild.id})`)  
-};
 
+};
 
 module.exports.help = {
 name: "ping"
