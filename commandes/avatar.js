4fe82147540bb846445fbc44867d389af1968671
Axteli <contact.axteli@gmail.com>
2020-10-27 12:22:55 +0100

    Avatar fix

--- commandes/avatar.js
+++ commandes/avatar.js
@@ -2,33 +2,41 @@ const Discord = require('discord.js')
 module.exports.run = async(bot, message, args) => {
 
     message.delete()
-    //si il n'y a pas de mention, executé cet embed (qui vas donne l'avatar de l'auteur du message)
-    if (!args[0]){
-        const avatar = new Discord.MessageEmbed()
-         .setTitle("Ton avatar")
-         .setURL(message.author.displayAvatarURL({ format: 'png', dynamic: true, size: 1024 }))
-         .setImage(message.author.displayAvatarURL({ format: 'png', dynamic: true, size: 1024 }))
-         .setDescription("Voici ton avatar")
-         .setColor("#527a9e")
-        return message.channel.send(avatar), console.log(`commande : avatar | par : ${message.author.tag} (${message.author.id}) | dans : ${message.channel.name} (${message.channel.id})| serveur : ${message.guild} (${message.guild.id})`)
-    }else{
-        //ou alors envoyé l'avatar de la personne qui a été mentionné
-        const user = message.mentions.users.first()
-        if (user === !'') {
-            const avatar1 = new Discord.MessageEmbed()
-             .setTitle(`Avatar de ${user.tag}`)
-             .setURL(user.displayAvatarURL({ format: 'png', dynamic: true, size: 1024 }))
-             .setImage(user.displayAvatarURL({ format: 'png', dynamic: true, size: 1024 }))
-             .setDescription(`Voici l'avatar de ${user.tag}`)
-             .setColor("#527a9e")
-             .setFooter(`${message.author.tag}`, message.author.displayAvatarURL({ format: 'png', dynamic: false, size: 1024 }))
-         console.log(`commande : avatar | par : ${message.author.tag} (${message.author.id}) | dans : ${message.channel.name} (${message.channel.id})| serveur : ${message.guild} (${message.guild.id}| membre visé : ${user})`)
-        return message.channel.send(avatar1)
-        }
+
+//si personne n'a été précisé
+    if (!args[0]) {
+        return message.channel.send(`<a:tickred:764793956813766687> Erreur | ${message.author.username}, tu n'as pas précisé de qui je dois afficher l'avatar!`),
+         console.log(`commande : avatar | par : ${message.author.tag} (${message.author.id}) | dans : ${message.channel.name} (${message.channel.id})| serveur : ${message.guild} (${message.guild.id})| détails : personne n'a été précisé`)
     }
     
+//ou alors envoyé l'avatar de la personne qui a été mentionné
+    const user = message.mentions.members.first() || message.guild.members.cache.get(args[0]);
 
-}
+//si l'utilsateur n'as pas été trouvé
+        if (!user) {
+            return message.channel.send(`<a:tickred:764793956813766687> Erreur | ${message.author.username}, je ne trouve pas cet utilisateur :/!`),
+             console.log(`commande : avatar | par : ${message.author.tag} (${message.author.id}) | dans : ${message.channel.name} (${message.channel.id})| serveur : ${message.guild} (${message.guild.id})| détails : utilisateur introuvable`)
+            
+            }else{
+
+        const avatar = new Discord.MessageEmbed()
+
+             .setTitle(`Avatar de ${user.user.tag}`)
+             .setURL(user.user.displayAvatarURL({ format: 'png', dynamic: true, size: 4096 }))
+             .setImage(user.user.displayAvatarURL({ format: 'png', dynamic: true, size: 4096 }))
+             .setDescription(`Voici l'avatar de ${user.user.tag}`)
+             .setColor("#527a9e")
+             .setFooter(`${message.author.tag}`, message.author.displayAvatarURL({ format: 'png', dynamic: false, size: 512 }))
+
+        console.log(`commande : avatar | par : ${message.author.tag} (${message.author.id}) | dans : ${message.channel.name} (${message.channel.id})| serveur : ${message.guild} (${message.guild.id}| membre visé : ${user})`)
+        message.channel.send(avatar)
+        }
+
+ }
+
+    
+
+
 module.exports.help = {
 name: "avatar"
 }
