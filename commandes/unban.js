436a5194c0e3927d17dbde0c06900e11caff91a1
Axteli <contact.axteli@gmail.com>
2020-10-29 10:11:48 +0100

    Unban fix

--- commandes/unban.js
+++ commandes/unban.js
@@ -1,16 +1,33 @@
 const Discord = require('discord.js')
 module.exports.run = async(bot, message, args) => {
 
-    const member = args[0];
 
-    if(!message.member.hasPermission("BAN_MEMBERS")) return message.channel.send('<a:tickred:764793956813766687> Erreur | Tu ne peux pas dé-bannir!'), console.log(`commande : unban | par : ${message.author.tag} (${message.author.id}) | dans : ${message.channel.name} (${message.channel.id})| serveur : ${message.guild} (${message.guild.id})| détails : ${message.author.tag} n'as pas la permission de dé-bannir`)
-    if(!message.guild.me.hasPermission("BAN_MEMBERS")) return message.channel.send('<a:tickred:764793956813766687> Erreur | Je n\'est pas la permission de dé-bannir !'), console.log(`commande : unban | par : ${message.author.tag} (${message.author.id}) | dans : ${message.channel.name} (${message.channel.id})| serveur : ${message.guild} (${message.guild.id})| détails : le bot n'as pas la permission de dé-bannir`)
 
+//vérifie les permission
+    if(!message.member.hasPermission("BAN_MEMBERS")) 
+        return message.channel.send('<a:tickred:764793956813766687> Erreur | Tu ne peux pas dé-bannir!'), 
+         console.log(`commande : unban | par : ${message.author.tag} (${message.author.id}) | dans : ${message.channel.name} (${message.channel.id})| serveur : ${message.guild} (${message.guild.id})| détails : ${message.author.username} n'as pas la permission de dé-bannir`)
+    
+    if(!message.guild.me.hasPermission("BAN_MEMBERS")) 
+        return message.channel.send('<a:tickred:764793956813766687> Erreur | Je n\'est pas la permission de dé-bannir !'),
+         console.log(`commande : unban | par : ${message.author.tag} (${message.author.id}) | dans : ${message.channel.name} (${message.channel.id})| serveur : ${message.guild} (${message.guild.id})| détails : le bot n'as pas la permission de dé-bannir`)
+
+//définir le membre a unban
+    const member = message.mentions.members.first() || message.guild.members.cache.get(args[0]);
+
+//si aucun membre reconnu
     if (!member) {
-         return message.channel.send(`<a:tickred:764793956813766687> Erreur | merci de donner un id!`)
+        return message.channel.send(`<a:tickred:764793956813766687> Erreur | ${message.author.username}, je ne trouve pas cet utilisateur !`),
+         console.log(`commande : ban | par : ${message.author.tag} (${message.author.id}) | dans : ${message.channel.name} (${message.channel.id})| serveur : ${message.guild} (${message.guild.id})| détails : l'utilisateur est introuvable`)
     }
-    if(!member.unbannable) return message.channel.send('<a:tickred:764793956813766687> Erreur | Je ne peux pas dé-bannir ce membre! Soit car l\'id est invalide, soit car cette personne n\'est pas banni.'),console.log(`commande : unban | par : ${message.author.tag} (${message.author.id}) | dans : ${message.channel.name} (${message.channel.id})| serveur : ${message.guild} (${message.guild.id})| détails : la personne ne peut pas etre unbanni`)
-    try {
+
+//s la personne ne peut pas etre unban
+    if(!member.unbannable) 
+        return message.channel.send(`<a:tickred:764793956813766687> Erreur | ${message.author.username}, Je ne peux pas dé-bannir ce membre! Soit car l\'id est invalide, soit car cette personne n\'est pas banni.`),
+         console.log(`commande : unban | par : ${message.author.tag} (${message.author.id}) | dans : ${message.channel.name} (${message.channel.id})| serveur : ${message.guild} (${message.guild.id})| détails : la personne ne peut pas etre unban`)
+    
+    
+        try {
         message.guild.fetchBans().then(bans => {
             message.guild.members.unban(member)
         })
