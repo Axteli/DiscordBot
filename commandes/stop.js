8abb5faa122fa528a1e3cf5c253d29516b5b6175
Axteli <contact.axteli@gmail.com>
2020-11-07 22:57:24 +0100

    Logs added on stop

--- commandes/stop.js
+++ commandes/stop.js
@@ -1,7 +1,7 @@
 const Discord = require('discord.js')
 var config = require('../config.json')
 const chalk = require('chalk')
-const log = console.log;
+const emote = require('../emote.json')
 module.exports.run = async(bot, message, args) => {    
     
 
@@ -12,19 +12,25 @@ module.exports.run = async(bot, message, args) => {
             await setTimeout(() => { msg.edit(`<:offline:764845548103532615> Le bot vas s'éteindre dans 3 secondes `);}, 2000);
             await setTimeout(() => { msg.edit(`<:offline:764845548103532615> Le bot vas s'éteindre dans 2 secondes `);}, 3000);
             await setTimeout(() => { msg.edit(`<:offline:764845548103532615> Le bot vas s'éteindre dans 1 seconde `);}, 4000);
-            await setTimeout(() => { msg.edit(`<:offline:764845548103532615> le bot s'éteint...`);}, 4090)
+            await setTimeout(() => { msg.edit(`<:offline:764845548103532615> le bot s'éteint...`);}, 5000)
 
 //changer le statut et envoyer un message dans la console
-        await setTimeout(() => { bot.user.setStatus('invisible'); }, 4980);
-        await log(chalk.bgRed(`le bot vas s'éteindre!`));
+        await setTimeout(() => { bot.user.setStatus('invisible'); }, 4900);
+        await console.log(chalk.bgRed(`le bot vas s'éteindre!`));
+
+        const channel = bot.channels.cache.get(`${config.logsChannel}`);
+        const embed = new Discord.MessageEmbed()
+        .setColor(`#00000`)
+        .setDescription(`${emote.status_offline} | Le bot vas s'éteindre!`)
+        .setTimestamp()
+        await channel.send(embed);
         
 //éteindre le bot
         await setTimeout(() => { process.exit(); }, 5010);
 
     } else {
 //envoyer le message d'erreur si la personne n'est pas owner du bot
-        message.channel.send(`<a:tickred:764793956813766687> Erreur | ${message.author.username}, tu ne peux pas éteindre le bot! \n||essayer d'éteindre le bot moi je dis ça mérite un ban,
-            après j'dis ça j'dis rien||`)
+        message.channel.send(`${emote.cross} Erreur | ${message.author.username}, tu ne peux pas éteindre le bot!`)
             console.log(`commande : stop | par : ${message.author.tag} (${message.author.id}) | dans : ${message.channel.name} (${message.channel.id})| serveur : ${message.guild} (${message.guild.id})| détails : ne peux pas éteindre le bot`);
     }
 
