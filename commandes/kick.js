145b2a7a145e9eff52ce604e55bd20d8d6cd7ffb
Axteli <contact.axteli@gmail.com>
2020-11-13 21:19:57 +0100

    Kick fix

--- commandes/kick.js
+++ commandes/kick.js
@@ -1,4 +1,6 @@
 const Discord = require('discord.js')
+const config= require('../config.json')
+const emote = require('../emote.json')
 module.exports.run = async(bot, message, args) => {
 
     message.delete()
