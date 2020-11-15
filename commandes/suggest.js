6d38b346d53c78a95a743e3c710b4ffffbfd1a81
Axteli <contact.axteli@gmail.com>
2020-11-15 14:34:56 +0100

    Suggest fix and update

--- commandes/suggest.js
+++ commandes/suggest.js
@@ -1,26 +1,53 @@
 const Discord = require('discord.js')
+const config = require('../config.json')
+const emote = require('../emote.json')
 module.exports.run = async(bot, message, args) => {
 
-    const icon = message.guild.iconURL()
     message.delete();
-    let suggestion = args.slice(0).join(" ");
-        console.log(`commande : suggest | par : ${message.author.tag} (${message.author.id}) | dans : ${message.channel.name} (${message.channel.id})| serveur : ${message.guild} (${message.guild.id})`)
-        let SuggestionChannel = message.guild.channels.cache.find(channel => channel.name === "shapez-dev");
-        if (!SuggestionChannel) return message.channel.send("Je ne trouve pas le salon suggestion :/");
-        if (!args[0]) return message.channel.send("Tu ne m'as pas dit quel étais ta suggestion!") 
-            console.log(`commande : suggest | par : ${message.author.tag} (${message.author.id}) | dans : ${message.channel.name} (${message.channel.id})| serveur : ${message.guild} (${message.guild.id})| suggestion : ${suggestion}`);
+
+    const suggestChannel = bot.channels.cache.get(config.suggestChannel)
+    const guild = suggestChannel.guild.id
+
+
+    if (`${message.guild.id}` === `${guild}`) {
+        
+
+        //définir suggestion comme args0
+        let suggestion = args.slice(0).join(" ");
+
+        //si aucune suggestion
+        if (!args[0]) 
+            return message.channel.send(`${emote.cross} Erreur | ${message.author.username}, tu ne m'as pas dit quel étais ta suggestion!`), 
+             console.log(`commande : suggest | par : ${message.author.tag} (${message.author.id}) | dans : ${message.channel.name} (${message.channel.id})| serveur : ${message.guild} (${message.guild.id})| détails : aucune suggestion`);
+        
+        //envoyer l'embed de suggestion   
+        const icon = message.guild.iconURL()
+
         const embed = new Discord.MessageEmbed()
-            .setAuthor(`${message.author.tag}`, message.author.displayAvatarURL({ format: 'png', dynamic: true, size: 1024 }))
-            .setDescription(suggestion)
-            .setColor("#527a9e")
-            .setFooter(`${message.guild.name}`, icon)
-            .setTimestamp()
-        SuggestionChannel.send(embed).then(msg => {
-            msg.react('764793938317803551');
-            msg.react('764793956813766687');
-                message.channel.send("<a:tickgreen:764793938317803551> ta suggestion a bien été envoyer dans ${SuggestionChannel} !");
+         .setAuthor(`${message.author.tag}`, message.author.displayAvatarURL({ format: 'png', dynamic: true, size: 1024 }))
+         .setDescription(suggestion)
+         .setColor("#527a9e")
+         .setFooter(`${message.guild.name}`, icon)
+         .setTimestamp()
+
+        suggestChannel.send(embed).then(msg => {
+        
+            //réagire a l'embed
+            msg.react(emote.tick);
+            msg.react(emote.neutral);
+            msg.react(emote.cross);
+
+            setTimeout(() => {
+                message.channel.send(`${emote.tick} | Ta suggestion a bien été envoyer dans ${suggestChannel} !`),
                 console.log(`commande : suggest | par : ${message.author.tag} (${message.author.id}) | dans : ${message.channel.name} (${message.channel.id})| serveur : ${message.guild} (${message.guild.id})| suggestion : ${suggestion}`)
+            }, 3000)
+
         });
+
+    }else{
+        return message.channel.send(`${emote.cross} Erreur | ${message.author.username}, le système de suggestion n'est pas disponible sur ce serveur!`),
+         console.log(`commande : suggest | par : ${message.author.tag} (${message.author.id}) | dans : ${message.channel.name} (${message.channel.id})| serveur : ${message.guild} (${message.guild.id})| détails : le système de suggestion n'est pas dispo sur ce serveur`);
+    }
 
     }
 
