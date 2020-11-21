23095020565cba71d400d730b8c8e58246757272
Axteli <contact.axteli@gmail.com>
2020-11-21 19:14:33 +0100

    channelinfo update

--- commandes/channelinfo.js
+++ commandes/channelinfo.js
@@ -1,54 +1,87 @@
 const Discord = require('discord.js')
+const emote = require('../emote.json')
+const moment = require('moment')
 module.exports.run = async(bot, message, args) => {
-   
 
-   message.delete();
-   const icon = message.guild.iconURL({ format: 'png', dynamic: true, size: 4096 })
+	message.delete();
 
-   const time = {
-      0: "00", 1:"01", 2:"02", 3:"03", 4:"04", 5:"05", 6:"06", 7:"07", 8:"08", 9:"09",
-      10:"10", 11:"11", 12:"12", 13:"13", 14:"14", 15:"15", 16:"16", 17:"17", 18:"18", 19:"19",
-      20:"20", 21:"21", 22:"22", 23:"23", 24:"24", 25:"25", 26:"26", 27:"27", 28:"28", 29:"29",
-      30:"30", 31:"31", 32:"32", 33:"33", 34:"34", 35:"35", 36:"36", 37:"37", 38:"38", 39:"39",
-      40:"40", 41:"41", 42:"42", 43:"43", 44:"44", 45:"45", 46:"46", 47:"47", 48:"48", 49:"49",
-      50:"50", 51:"51", 52:"52", 53:"53", 54:"54", 55:"55", 56:"56", 57:"57", 58:"58",59:"59"
-   }
+   	const icon = message.guild.iconURL({ format: 'png', dynamic: true, size: 4096 })
+
+	//défnir le channel
+	if (!args[0]) {
+		var channel = message.channel;
+	}else{
+		var channel = message.mentions.channels.first() || message.guild.channels.cache.get(args[0])
+	}
 
 
-   var topic = [];
+	//si aucun channel n'a été définis alors erreur
+	if (!channel) {
+		return message.channel.send(`${emote.cross} Erreur | ${message.author.username}, je ne trouve pas le salon!`),
+		console.log(`commande : channelinfo | par : ${message.author.tag} (${message.author.id}) | dans : ${message.channel.name} (${message.channel.id})| serveur : ${message.guild} (${message.guild.id}) | détails : salon introuvable`)
+	}
 
-   if(!message.channel.topic) {
-      topic.push('aucune')
-   }else{
-      topic.push(`${message.channel.topic}`)
-   }
-      
+	if(channel.type === 'text' || channel.type === 'news' || channel.type === 'store') {
 
-   var a = new Date(message.channel.createdTimestamp);
-   var months = ['Janvier','Fevrier','Mars','Avril','Mai','Juin','Juillet','Août','Septembre','Octobre','Novembre','Décembre'];
-   var year = a.getFullYear();
-   var month = months[a.getMonth()];
-   var date = a.getDate();
-   var hour = a.getHours();
-   var min = a.getMinutes();
-   var sec = a.getSeconds();
-   var createdAt = (`le ${time[date]} ${month} ${year} à ${time[hour]}:${time[min]}:${time[sec]}`)
-
-
-   const embed = new Discord.MessageEmbed()
-    .setColor("#527a9e")
-    .setTitle(`Information sur le salon : ${message.channel.name}`)
-    .addField(`🔧 | Description`, topic, false)
-    .addField(`📃 | Nom`, message.channel.name, true)
-    .addField(`🆔 | Id`, message.channel.id, true)
-    .addField(`🔞 | NSFW`, message.channel.nsfw ? `oui` : `non`, true)
-    .addField(`📙 | Catégorie`, `${message.channel.parent}\n(${message.channel.parentID})`, true)
-    .addField(`📆 | Date de création`, createdAt, false)
-    .setThumbnail(icon)
+	
+   		const embed = new Discord.MessageEmbed()
+    	 .setColor("#527a9e")
+    	 .setTitle(`Information sur le salon textuel : ${channel.name}`)
+    	 .addField(`🔧 | Description`, channel.topic !== null ? channel.topic : 'Aucune', false)
+    	 .addField(`📃 | Nom`, channel.name, true)
+    	 .addField(`🆔 | Id`, channel.id, true)
+		 .addField(`🔞 | NSFW`, channel.nsfw ? `oui` : `non`, true)
+		 .addField(`📙 | Catégorie`, `${channel.parent !== null ? channel.parent : 'non-catégorisé'}
+		 	${channel.parentID !== null ? `(${channel.parentID})` : ''}`, true)
+		 .addField(`🎚 | Position dans la catégorie`, channel.position + 1, true)
+		 .addField(`📆 | Date de création`, moment(channel.createdAt).format('[le] DD/MM/YYYY [à] HH:mm:ss'), false)
+		 .setThumbnail(icon)
+		 .setFooter(message.member.user.username, message.member.user.displayAvatarURL())
        
-   message.channel.send(embed)
-   return console.log(`commande : channelinfo | par : ${message.author.tag} (${message.author.id}) | dans : ${message.channel.name} (${message.channel.id})| serveur : ${message.guild} (${message.guild.id})`)
-   }
+   		message.channel.send(embed)
+   		return console.log(`commande : channelinfo | par : ${message.author.tag} (${message.author.id}) | dans : ${message.channel.name} (${message.channel.id})| serveur : ${message.guild} (${message.guild.id}) | salon textuel : ${channel.name}(${channel.id})`)
+	}
+	
+	if (channel.type === 'category') {
+
+
+		const embed = new Discord.MessageEmbed()
+		 .setColor("#527a9e")
+		 .setTitle(`Information sur la catégorie : ${channel.name}`)
+		 .addField(`📃 | Nom`, channel.name, true)
+		 .addField(`🆔 | Id`, channel.id, true)
+		 .addField(`🛋 | Salons`, channel.children.size, false)
+		 .addField(`🎚 | Position`, channel.rawPosition, false)
+		 .addField(`📆 | Date de création`, moment(channel.createdAt).format('[le] DD/MM/YYYY [à] HH:MM:SS'))
+		 .setThumbnail(icon)
+		 .setFooter(message.member.user.username, message.member.user.displayAvatarURL())
+		message.channel.send(embed)
+		return console.log(`commande : channelinfo | par : ${message.author.tag} (${message.author.id}) | dans : ${message.channel.name} (${message.channel.id})| serveur : ${message.guild} (${message.guild.id}) | catégorie : ${channel.name}(${channel.id})`)
+	}
+
+
+	if(channel.type === 'voice') {
+
+
+		const embed = new Discord.MessageEmbed()
+		 .setThumbnail(icon)
+		 .setColor("#527a9e")
+		 .setTitle('Information sur le channel vocal : ' + channel.name)
+		 .addField(`📃 | Nom`, channel.name, true)
+		 .addField(`🆔 | Id`, channel.id, true)
+		 .addField('📦 | Débit binaire (bitrate)', channel.bitrate / 1000 + 'kbps', true)
+		 .addField(`🎤 | Membres connectés`, channel.members.size, false)
+		 .addField(`⛔ | Limite d'utilisateur connecté`, channel.userLimit === 0 ? 'aucune' : channel.userLimit, true)
+		 .addField(`📆 | Date de création`, moment(channel.createdAt).format('[le] DD/MM/YYYY [à] HH:MM:SS'), false)
+		 .setFooter(message.member.user.username, message.member.user.displayAvatarURL())
+		message.channel.send(embed)
+		return console.log(`commande : channelinfo | par : ${message.author.tag} (${message.author.id}) | dans : ${message.channel.name} (${message.channel.id})| serveur : ${message.guild} (${message.guild.id}) | salon vocal : ${channel.name}(${channel.id})`)
+	}
+
+
+	return message.channel.send(`${emote.cross} Erreur | ${message.author.username}, je ne trouve pas le type du salon! Il m'est donc impossible d'affiché ses informations.`),
+	 console.log(`commande : channelinfo | par : ${message.author.tag} (${message.author.id}) | dans : ${message.channel.name} (${message.channel.id})| serveur : ${message.guild} (${message.guild.id}) | détails : type du channel introuvable`)
+}
 
 module.exports.help = {
 name: "channelinfo"
