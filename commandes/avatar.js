714c2b3551145e08cc561b5040b9dc3e72514add
Axteli <contact.axteli@gmail.com>
2020-11-25 14:05:48 +0100

    avatar : emoji fixé et information pour la commande help ajouté

--- commandes/avatar.js
+++ commandes/avatar.js
@@ -1,4 +1,5 @@
 const Discord = require('discord.js')
+const emote = require('../emote.json')
 module.exports.run = async(bot, message, args) => {
 
     message.delete()
@@ -17,7 +18,7 @@ module.exports.run = async(bot, message, args) => {
 
 
     //si args0 est égal a server alors montré l'avatar du serveur
-    if (args[0] === 'server') {
+    if (args[0] === 'server' || args[0] === 'serveur') {
         const avatar = new Discord.MessageEmbed()
 
          .setTitle(`Avatar du serveur`)
@@ -38,7 +39,7 @@ module.exports.run = async(bot, message, args) => {
 
     //si l'utilsateur n'as pas été trouvé
     if (!user) {
-        return message.channel.send(`<a:tickred:764793956813766687> Erreur | ${message.author.username}, je ne trouve pas cet utilisateur :/!`),
+        return message.channel.send(`${emote.cross} Erreur | ${message.author.username}, je ne trouve pas cet utilisateur :/!`),
          console.log(`commande : avatar | par : ${message.author.tag} (${message.author.id}) | dans : ${message.channel.name} (${message.channel.id})| serveur : ${message.guild} (${message.guild.id})| détails : utilisateur introuvable`)
 
     }else{
@@ -51,13 +52,15 @@ module.exports.run = async(bot, message, args) => {
          .setDescription(`Voici l'avatar de ${user.user.tag}`)
          .setColor("#527a9e")
          .setFooter(`${message.author.tag}`, message.author.displayAvatarURL({ format: 'png', dynamic: false, size: 512 }))
-
-        console.log(`commande : avatar | par : ${message.author.tag} (${message.author.id}) | dans : ${message.channel.name} (${message.channel.id})| serveur : ${message.guild} (${message.guild.id}| membre visé : ${user})`)
         message.channel.send(avatar)
-        }
+        console.log(`commande : avatar | par : ${message.author.tag} (${message.author.id}) | dans : ${message.channel.name} (${message.channel.id})| serveur : ${message.guild} (${message.guild.id}| membre visé : ${user})`)
+    }
 
 }
 
 module.exports.help = {
-name: "avatar"
+    name: "avatar",
+    description: "Affiche l'avatar d'un membre ou du serveur.",
+    usage: "avatar [membre/serveur]",
+    example: ["avatar @Axtéli", "avatar", "avatar serveur"]
 }
