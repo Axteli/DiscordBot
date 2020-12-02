acffe3dce8f21675bfcc9b38a503ebf5fc50fe86
Axteli <contact.axteli@gmail.com>
2020-12-02 20:25:05 +0100

    message de confirmation de la suggestion fixé

--- commandes/suggest.js
+++ commandes/suggest.js
@@ -10,7 +10,7 @@ module.exports.run = async(bot, message, args) => {
 
 
     if (`${message.guild.id}` === `${guild}`) {
-        
+
 
         //définir suggestion comme args0
         let suggestion = args.slice(0).join(" ");
@@ -19,38 +19,41 @@ module.exports.run = async(bot, message, args) => {
         if (!args[0]) 
             return message.channel.send(`${emote.cross} Erreur | ${message.author.username}, tu ne m'as pas dit quel étais ta suggestion!`), 
              console.log(`commande : suggest | par : ${message.author.tag} (${message.author.id}) | dans : ${message.channel.name} (${message.channel.id})| serveur : ${message.guild} (${message.guild.id})| détails : aucune suggestion`);
-        
+
         //envoyer l'embed de suggestion   
         const icon = message.guild.iconURL()
 
         const embed = new Discord.MessageEmbed()
-         .setAuthor(`${message.author.tag}`, message.author.displayAvatarURL({ format: 'png', dynamic: true, size: 1024 }))
+         .setTitle(`Suggestion`)
          .setDescription(suggestion)
          .setColor(config.embedColor)
-         .setFooter(`${message.guild.name}`, icon)
-         .setTimestamp()
+         .setFooter(`${message.author.tag}`, message.author.displayAvatarURL({ format: 'png', dynamic: true, size: 1024 }))
 
         suggestChannel.send(embed).then(msg => {
-        
+
             //réagire a l'embed
             msg.react(emote.tick);
             msg.react(emote.neutral);
-            msg.react(emote.cross);
-
-            setTimeout(() => {
+            msg.react(emote.cross)
+            //envoyé le message de confirmation une fois les réactions ajoutés
+            .then(reaction =>{
                 message.channel.send(`${emote.tick} | Ta suggestion a bien été envoyer dans ${suggestChannel} !`),
-                console.log(`commande : suggest | par : ${message.author.tag} (${message.author.id}) | dans : ${message.channel.name} (${message.channel.id})| serveur : ${message.guild} (${message.guild.id})| suggestion : ${suggestion}`)
-            }, 3000)
+                 console.log(`commande : suggest | par : ${message.author.tag} (${message.author.id}) | dans : ${message.channel.name} (${message.channel.id})| serveur : ${message.guild} (${message.guild.id})| suggestion : ${suggestion}`)
+            })
 
-        });
+        })
 
     }else{
         return message.channel.send(`${emote.cross} Erreur | ${message.author.username}, le système de suggestion n'est pas disponible sur ce serveur!`),
          console.log(`commande : suggest | par : ${message.author.tag} (${message.author.id}) | dans : ${message.channel.name} (${message.channel.id})| serveur : ${message.guild} (${message.guild.id})| détails : le système de suggestion n'est pas dispo sur ce serveur`);
     }
 
-    }
+}
 
-    module.exports.help = {
-    name: "suggest"
-    }
+module.exports.help = {
+    name: "suggest",
+    aliases: "suggestion",
+    description: "Envoie votre suggestion dans la liste des suggestions du serveur.",
+    usage: "suggest <suggestion>",
+    example: "suggest Ma suggestion"
+}
