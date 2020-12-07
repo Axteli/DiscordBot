821947f32a7c9f7a2875a6b929a7e9f026a1d337
Axteli <contact.axteli@gmail.com>
2020-12-07 21:10:47 +0100

    event ready: visibilité du code et petit changement

--- event/ready.js
+++ event/ready.js
@@ -7,6 +7,8 @@ module.exports = (bot, message) => {
     console.log(`------------------------`)
     console.log(bot.user.username + " est en ligne !")
 
+
+
     var jeuxs = [
         config.status.one,
         config.status.two,
@@ -15,7 +17,7 @@ module.exports = (bot, message) => {
         `Prefixe : ${config.prefix}`
     ];
 
-
+    
     setInterval(function () {
 
         var jeux = jeuxs[Math.floor(Math.random() * jeuxs.length)];
@@ -24,27 +26,62 @@ module.exports = (bot, message) => {
 
             {type: config.status.type, 
             url: `https://twitch.tv/${bot.user.username}`}
-        );
+        )
+        
 
-    }, 4000);
-    console.log(`Le status est optérationnel!`)
+    }, 4000)
+    console.log(`Le statut est optérationnel!`)
+
+
+
 
     if(config.deleteCommands === 'yes' || config.deleteCommands === 'no') {
     }else{
         console.error(chalk.red("la valeur 'deleteCommands' n'est pas définis sur 'yes' ou 'no', les commandes ne seront donc pas supprimé!"))
     }
 
-    const channel = bot.channels.cache.get(`${config.logsChannel}`);
-	if(!channel) {
+
+    const suggestChannel = bot.channels.cache.get(config.suggestChannel);
+    if(!suggestChannel) {
+        console.error(chalk.red('Je ne trouve pas le salon de suggestion!'))
+        console.log('Je vais faire un gâteau pour me faire pardonner')
+    }
+
+
+    const owner1 = bot.users.cache.get(config.owner1)
+    const owner2 = bot.users.cache.get(config.owner2)
+
+    if(!owner1 && !owner2) {
+        console.error(chalk.red('Je ne trouve pas les owners 1 et 2!'))
+    }else{
+
+        if(!owner1) {
+            console.error(chalk.red('Je ne trouve pas l\'owner1!'))
+        }
+
+        if(!owner2) {
+            console.error(chalk.red('Je ne trouve pas l\'owner2!'))
+        }
+    }
+
+
+    const logsChannel = bot.channels.cache.get(`${config.logsChannel}`);
+
+	if(!logsChannel) {
 		console.error(chalk.red("je ne trouve pas le salon de logs!"))
 	}else{
+
         const embed = new Discord.MessageEmbed()
          .setColor(`GREEN`)
          .setDescription(`${emote.status.online} | Le bot est allumé!`)
          .setTimestamp()
-        channel.send(embed);
-        console.log('log envoyé!')
-	}
+        logsChannel.send(embed)
+        .then(msg => {
+            console.log('log de démarrage envoyé!')
+        })
+    }
+    
+
     console.log(chalk.blue(`----------INFO----------`))
 
     console.log(`\nServeurs :`)
@@ -55,17 +92,13 @@ module.exports = (bot, message) => {
 
     console.log(`\nBot:\n    ${bot.user.username} - ${bot.user.id}`)
 
-    const owner1 = bot.users.cache.get(config.owner1)
-    const owner2 = bot.users.cache.get(config.owner2)
 
-    if (!owner1 || !owner2) {
-        console.error(chalk.red("je n'ai pas trouvé les owners! Soit car je n'ai pas de serveurs en commun avec eux, soit car l'id est invalide"))
-    }else{
+    if(owner1 && owner2) {
         console.log(`\nConfiguration:\n   Préfixe: ${config.prefix} - Administrateur1: ${owner1.tag} (${owner1.id}) - Administrateur2: ${owner2.tag} (${owner2.id})`)
     }
 
     
 
-    console.log(chalk.cyan(`\nLogs de commandes:\n`))
+    console.log(chalk.blue(`\n------------------------\n`))
 
 }
