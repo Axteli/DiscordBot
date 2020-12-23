efdae051a5f581c4740b6d085edd174d385a0c7b
Axteli <contact.axteli@gmail.com>
2020-12-24 00:32:59 +0100

    code des handlers nettoyé

--- handlers/command.js
+++ handlers/command.js
@@ -1,22 +1,24 @@
-const fs = require('fs')
-const chalk = require('chalk')
+const { readdirSync } = require('fs');
+const chalk = require('chalk');
+
 module.exports = (bot) => {
 
-	console.log(chalk.magenta(`Chargement des commandes...`))
-	fs.readdirSync("./commandes/").map(category => {
 
-		fs.readdirSync(`./commandes/${category}/`).map(files => {
+	console.log(chalk.magenta(`Chargement des commandes...`));
+	readdirSync("./commandes/").map(category => {
+
+		readdirSync(`./commandes/${category}/`).map(files => {
 
 
+			let props = require(`../commandes/${category}/${files}`);
+			bot.commands.set(props.help.name, props);
+			bot.aliases.set(props.help.aliases, props);
+			console.log(`${files} chargée!`);
 
-        	let props = require(`../commandes/${category}/${files}`);
-      		bot.commands.set(props.help.name, props);
-      		bot.aliases.set(props.help.aliases, props)
-      		console.log(`${files} chargée!`);
 
-	
-		})
+		});
+
 	});
 	console.log(chalk.bgMagenta(`commandes chargées!`));
 
-}
+};
--- handlers/event.js
+++ handlers/event.js
@@ -1,36 +1,45 @@
-const fs = require('fs')
-const chalk = require('chalk')
-const logs = require('../config/logs.json')
+const { readdirSync } = require('fs');
+const chalk = require('chalk');
+const logs = require('../config/logs.json');
+
 module.exports = (bot) => {
 
-	console.log(chalk.green(`Chargement des events...`))
-    fs.readdirSync("./event/").map (category => {
+	console.log(chalk.green(`Chargement des events...`));
+	readdirSync("./event/").map(category => {
 
-		fs.readdirSync(`./event/${category}/`).map(files => {
+		readdirSync(`./event/${category}/`).map(files => {
 
 
-			const evt = require(`../event/${category}/${files}`)
+			const evt = require(`../event/${category}/${files}`);
 			const evtName = files.split(".")[0];
 
-			if(logs[evtName]) {
 
-				if(logs[evtName] === 'true') {
+			if (logs[evtName]) {
+
+				if (logs[evtName] === 'true') {
+
 
 					bot.on(evtName, evt.bind(null, bot));
 					console.log(`${files} chargé!`);
 
-				}else{
-					console.log(files + ` non chargé!`)
-				}
 
-			}else{
+				} else {
+
+					console.log(files + ` non chargé!`)
+
+				};
+
+			} else {
+
 				bot.on(evtName, evt.bind(null, bot));
 				console.log(`${files} chargé!`);
-			}
 
-    	});
+			};
+
+
+		});
 
 	});
 	console.log(chalk.bgGreen(`events chargées!`));
 
-}
+};
