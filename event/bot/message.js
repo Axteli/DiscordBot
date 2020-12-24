1bfbfbbfc24476bd7aa952f30081006d5473ac43
Axteli <contact.axteli@gmail.com>
2020-12-24 15:04:08 +0100

    optimisation des events bot

--- event/bot/message.js
+++ event/bot/message.js
@@ -1,32 +1,41 @@
-const Discord = require('discord.js')
-const config = require('../../config/config.json')
-const chalk = require('chalk')
+const { prefix, deleteCommands } = require('../../config/config.json');
+const { red } = require('chalk');
+
 module.exports = (bot, message) => {
 
 
-    const prefix = config.prefix
 
-    if(message.author.bot) return;
-    if(message.channel.type === 'dm') return;
+	if (message.author.bot) return;
+	if (message.channel.type === 'dm') return;
 
-    if(message.content === `<@!${bot.user.id}>`) {
-        return message.channel.send(`Hey ! Mon préfixe est : \`${prefix}\``)
-    }
-    
-    if (!message.content.startsWith(config.prefix)) return
+
+	if (message.content === `<@!${bot.user.id}>`) {
+		return message.channel.send(`Hey ! Mon préfixe est : \`${prefix}\``);
+	};
 
 
 
-    let content = message.content.split(" ");
-    let command = content[0];
-    let args = content.slice(1);
-  
-  
-    let commandfile = bot.commands.get(command.slice(prefix.length)) || bot.aliases.get(command.slice(prefix.length))
-    if(commandfile) {
-        if(config.deleteCommands === 'yes') {
-            message.delete().catch(err => {console.error(chalk.red('Je n\'est pas réussi à supprimer la commande! erreur: '+err))})
-        }    
-        commandfile.run(bot,message,args);
-    }
-}
+	if (!message.content.startsWith(prefix)) return;
+
+
+	let content = message.content.split(" ");
+	let command = content[0];
+	let args = content.slice(1);
+
+
+	let commandfile = bot.commands.get(command.slice(prefix.length)) || bot.aliases.get(command.slice(prefix.length));
+	
+	if (commandfile) {
+
+
+		if (deleteCommands === 'yes') {
+
+			message.delete().catch(err => { console.error(red('Je n\'est pas réussi à supprimer la commande! erreur: ' + err)); });
+
+		};
+
+		commandfile.run(bot, message, args);
+
+
+	};
+};
--- event/bot/ready.js
+++ event/bot/ready.js
@@ -1,104 +1,140 @@
-const Discord = require('discord.js')
-const chalk = require('chalk')
+const { MessageEmbed } = require('discord.js')
+const { red, blue } = require('chalk')
 const config = require('../../config/config.json')
-const emote = require('../../config/emote.json')
-module.exports = (bot, message) => {
+const { online } = require('../../config/emote.json')
 
-    console.log(`------------------------`)
-    console.log(bot.user.username + " est en ligne !")
+module.exports = (bot) => {
 
 
 
-    var jeuxs = [
-        config.status.one,
-        config.status.two,
-        config.status.three,
-        config.status.four,
-        `Prefixe : ${config.prefix}`
-    ];
-
-    
-    setInterval(function () {
-
-        var jeux = jeuxs[Math.floor(Math.random() * jeuxs.length)];
-
-        bot.user.setActivity(jeux, 
-
-            {type: config.status.type, 
-            url: `https://twitch.tv/${bot.user.username}`}
-        )
-        
-
-    }, 4000)
-    console.log(`Le statut est optérationnel!`)
+	console.log(`------------------------`);
+	console.log(bot.user.username + " est en ligne !");
 
 
 
-
-    if(config.deleteCommands === 'yes' || config.deleteCommands === 'no') {
-    }else{
-        console.error(chalk.red("la valeur 'deleteCommands' n'est pas définis sur 'yes' ou 'no', les commandes ne seront donc pas supprimé!"))
-    }
-
-
-    const suggestChannel = bot.channels.cache.get(config.suggestChannel);
-    if(!suggestChannel) {
-        console.error(chalk.red('Je ne trouve pas le salon de suggestion!'))
-        console.log('Je vais faire un gâteau pour me faire pardonner')
-    }
+	//définir le statut et sa boucle
+	var jeuxs = [
+		config.status.one,
+		config.status.two,
+		config.status.three,
+		config.status.four,
+		`Prefixe : ${config.prefix}`
+	];
 
 
-    const owner1 = bot.users.cache.get(config.owner1)
-    const owner2 = bot.users.cache.get(config.owner2)
 
-    if(!owner1 && !owner2) {
-        console.error(chalk.red('Je ne trouve pas les owners 1 et 2!'))
-    }else{
-
-        if(!owner1) {
-            console.error(chalk.red('Je ne trouve pas l\'owner1!'))
-        }
-
-        if(!owner2) {
-            console.error(chalk.red('Je ne trouve pas l\'owner2!'))
-        }
-    }
+	setInterval(function () {
 
 
-    const logsChannel = bot.channels.cache.get(`${config.logsChannel}`);
+		var jeux = jeuxs[Math.floor(Math.random() * jeuxs.length)];
 
-	if(!logsChannel) {
-		console.error(chalk.red("je ne trouve pas le salon de logs!"))
-	}else{
+		bot.user.setActivity(jeux,
 
-        const embed = new Discord.MessageEmbed()
-         .setColor(`GREEN`)
-         .setDescription(`${emote.status.online} | Le bot est allumé!`)
-         .setTimestamp()
-        logsChannel.send(embed)
-        .then(msg => {
-            console.log('log de démarrage envoyé!')
-        })
-    }
-    
-
-    console.log(chalk.blue(`----------INFO----------`))
-
-    console.log(`\nServeurs :`)
-
-    bot.guilds.cache.forEach((guild) => {
-        console.log(`   - ${guild.name} - ${guild.id} - admin : ${guild.me.hasPermission("ADMINISTRATOR") ? `oui` : `non`}`)
-    })
-
-    console.log(`\nBot:\n    ${bot.user.username} - ${bot.user.id}`)
+			{
+				type: config.status.type,
+				url: `https://twitch.tv/${bot.user.username}`
+			}
+		);
 
 
-    if(owner1 && owner2) {
-        console.log(`\nConfiguration:\n   Préfixe: ${config.prefix} - Administrateur1: ${owner1.tag} (${owner1.id}) - Administrateur2: ${owner2.tag} (${owner2.id})`)
-    }
+	}, 4000);
+	console.log(`Le statut est optérationnel!`);
 
-    
 
-    console.log(chalk.blue(`\n------------------------\n`))
 
-}
+	//vérifie la configuration du deleteCommands
+	if (config.deleteCommands === 'yes' || config.deleteCommands === 'no') {
+
+	} else {
+
+		console.error(red("la valeur 'deleteCommands' n'est pas définis sur 'yes' ou 'no', les commandes ne seront donc pas supprimé!"));
+
+	};
+
+
+	//vérifie la configuration du suggestChannel
+	const suggestChannel = bot.channels.cache.get(config.suggestChannel);
+
+	if (!suggestChannel) {
+
+		console.error(red('Je ne trouve pas le salon de suggestion!'));
+		console.log('Je vais faire un gâteau pour me faire pardonner');
+
+	};
+
+
+	//vérifie la configuration des owners
+	const owner1 = bot.users.cache.get(config.owner1);
+	const owner2 = bot.users.cache.get(config.owner2);
+
+
+	if (!owner1 && !owner2) {
+
+		console.error(red('Je ne trouve pas les owners 1 et 2!'));
+
+	} else {
+
+		if (!owner1) {
+
+			console.error(red('Je ne trouve pas l\'owner1!'));
+
+		};
+
+		if (!owner2) {
+
+			console.error(red('Je ne trouve pas l\'owner2!'));
+
+		};
+	};
+
+
+	//vérifie la configuration du logsChannel et envoie le log de démarrage
+	const logsChannel = bot.channels.cache.get(`${config.logsChannel}`);
+
+	if (!logsChannel) {
+
+		console.error(red("je ne trouve pas le salon de logs!"));
+
+	} else {
+
+		const embed = new MessageEmbed()
+			.setColor(`GREEN`)
+			.setDescription(`${online} | Le bot est allumé!`)
+			.setTimestamp();
+
+
+		logsChannel.send(embed).then(
+
+			console.log('log de démarrage envoyé!')
+
+		);
+
+	};
+
+
+	console.log(blue(`----------INFO----------`));
+
+	console.log(`\nServeurs :`);
+
+
+	//écrit la liste des serveurs sur lesquels ce trouve le bot
+	bot.guilds.cache.forEach((guild) => {
+
+		console.log(`   - ${guild.name} - ${guild.id} - admin : ${guild.me.hasPermission("ADMINISTRATOR") ? `oui` : `non`}`);
+
+	});
+
+
+
+	console.log(`\nBot:\n    ${bot.user.username} - ${bot.user.id}`);
+
+
+	if (owner1 && owner2) {
+		console.log(`\nConfiguration:\n   Préfixe: ${config.prefix} - Administrateur1: ${owner1.tag} (${owner1.id}) - Administrateur2: ${owner2.tag} (${owner2.id})`);
+	};
+
+
+
+	console.log(blue(`\n------------------------\n`));
+
+};
